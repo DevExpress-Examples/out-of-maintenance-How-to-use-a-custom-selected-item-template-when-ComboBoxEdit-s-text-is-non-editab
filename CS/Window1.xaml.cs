d73ac5e89c9edb1591ced85febf7c49022b00646
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace TextEdit_EditNonEditableTemplate {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            List<Employee> data = new List<Employee>();
+            data.Add(new Employee { Name = "Steven", CardColor = "Red" });
+            data.Add(new Employee { Name = "Mary", CardColor = "Green" });
+            data.Add(new Employee { Name = "John", CardColor = "Blue" });
+            data.Add(new Employee { Name = "Claudia", CardColor = "Red" });
+            gridControl1.DataSource = data;
+        }
+    }
+    public class Employee {
+        public string Name { get; set; }
+        public string CardColor { get; set; }
+    }
+}
