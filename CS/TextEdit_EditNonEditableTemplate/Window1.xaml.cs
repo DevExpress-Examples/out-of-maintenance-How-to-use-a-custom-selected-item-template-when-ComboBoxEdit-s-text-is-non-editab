511da8d4c8d80ceec0902e9dbd0955b5d1fd3d3f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/TextEdit_EditNonEditableTemplate/Window1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace TextEdit_EditNonEditableTemplate {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+        private void gridControl1_Loaded(object sender, RoutedEventArgs e) {
+            List<Employee> data = new List<Employee>();
+            data.Add(new Employee { Name = "Steven", CardColor = "Red" });
+            data.Add(new Employee { Name = "Mary", CardColor = "Green" });
+            data.Add(new Employee { Name = "John", CardColor = "Blue" });
+            data.Add(new Employee { Name = "Claudia", CardColor = "Red" });
+
+            gridControl1.DataSource = data;
+         }
+    }
+    public class Employee {
+        public string Name { get; set; }
+        public string CardColor { get; set; }
+    }
+}
---
+++ VB/TextEdit_EditNonEditableTemplate/Window1.xaml.vb
@@ -0,0 +1,41 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Collections.Generic
+Imports System.Windows
+
+Namespace TextEdit_EditNonEditableTemplate
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub gridControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim data As New List(Of Employee)()
+			data.Add(New Employee With {.Name = "Steven", .CardColor = "Red"})
+			data.Add(New Employee With {.Name = "Mary", .CardColor = "Green"})
+			data.Add(New Employee With {.Name = "John", .CardColor = "Blue"})
+			data.Add(New Employee With {.Name = "Claudia", .CardColor = "Red"})
+
+			gridControl1.DataSource = data
+		End Sub
+	End Class
+	Public Class Employee
+		Private privateName As String
+		Public Property Name() As String
+			Get
+				Return privateName
+			End Get
+			Set(ByVal value As String)
+				privateName = value
+			End Set
+		End Property
+		Private privateCardColor As String
+		Public Property CardColor() As String
+			Get
+				Return privateCardColor
+			End Get
+			Set(ByVal value As String)
+				privateCardColor = value
+			End Set
+		End Property
+	End Class
+End Namespace
