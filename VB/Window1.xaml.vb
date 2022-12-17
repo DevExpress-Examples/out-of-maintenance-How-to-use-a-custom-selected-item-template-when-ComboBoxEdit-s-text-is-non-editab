1e43a7e5c87a9ae3f8e8c4f7c8a6e7b4237c58bd
DevExpressExampleBot <fakeEmail>
2022-12-17 13:47:21 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,40 +1,33 @@
-﻿Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
 Imports System.Windows
 
 Namespace TextEdit_EditNonEditableTemplate
-	Partial Public Class Window1
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class Window1
+        Inherits Window
 
-	Public Class ViewModel
-		Public Property Data() As ObservableCollection(Of Employee)
-		Public Sub New()
-			Data = New ObservableCollection(Of Employee)()
-			Data.Add(New Employee With {
-				.Name = "Steven",
-				.CardColor = "Red"
-			})
-			Data.Add(New Employee With {
-				.Name = "Mary",
-				.CardColor = "Green"
-			})
-			Data.Add(New Employee With {
-				.Name = "John",
-				.CardColor = "Blue"
-			})
-			Data.Add(New Employee With {
-				.Name = "Claudia",
-				.CardColor = "Red"
-			})
-		End Sub
-	End Class
-	Public Class Employee
-		Public Property Name() As String
-		Public Property CardColor() As String
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+
+    Public Class ViewModel
+
+        Public Property Data As ObservableCollection(Of Employee)
+
+        Public Sub New()
+            Data = New ObservableCollection(Of Employee)()
+            Data.Add(New Employee With {.Name = "Steven", .CardColor = "Red"})
+            Data.Add(New Employee With {.Name = "Mary", .CardColor = "Green"})
+            Data.Add(New Employee With {.Name = "John", .CardColor = "Blue"})
+            Data.Add(New Employee With {.Name = "Claudia", .CardColor = "Red"})
+        End Sub
+    End Class
+
+    Public Class Employee
+
+        Public Property Name As String
+
+        Public Property CardColor As String
+    End Class
 End Namespace
